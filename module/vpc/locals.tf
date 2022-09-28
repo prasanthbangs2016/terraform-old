83d722e22db28a5561b4a028a861fdb8c7148142
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-28 21:07:17 +0530

    added vpc

--- module/vpc/locals.tf
+++ module/vpc/locals.tf
@@ -1,6 +1,6 @@
 locals {
   VPC_NAME  = "${var.TAGS["project"]}-${var.TAGS["ENV"]}-VPC"
   IAC_TOLL  = var.TAGS["IAC_TOOL"]
-  GIT_REPO  = var.tags["GIT_REPO"]
+  GIT_REPO  = var.TAGS["GIT_REPO"]
 
 }
